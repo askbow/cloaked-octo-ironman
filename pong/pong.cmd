b9f651a5ac8c21b859ce0c47c61f4082c61ca722
Denis Borchev <dborchev@gmail.com>
2014-12-17 13:22:56 +0300

    pong --- added more hosts, better comments
    added lines for several more LEVEL3 hosts
    added a line for MSK-IX time server (commented out, as it's not a good
    practice not it is polite to ping NTP servers often)

--- pong/pong.cmd
+++ pong/pong.cmd
@@ -6,6 +6,10 @@ echo ==========================
 echo PING LEVEL3
 ping 4.2.2.1
 ping 4.2.2.2
+rem ping 4.2.2.3
+rem ping 4.2.2.4
+rem ping 4.2.2.5
+rem ping 4.2.2.6
 echo ==========================
 echo PING AUSTRALIA ns1.telstra.net
 ping 139.130.4.5
@@ -14,13 +18,18 @@ echo PING OpenDNS
 ping 208.67.222.222
 ping 208.67.220.220
 echo ==========================
+echo PING MSK-IX NTP stratum-1 server --- better not to do this often
+rem ping 194.190.168.1
+echo ==========================
+echo next tests require DNS to be working
+echo ==========================
 echo PING YANDEX
 ping ya.ru
 echo ==========================
 echo PING INFOBOX:
 echo ------------- AMSTERDAM (EUROPE)
 ping ams.sandbox.infobox.ru
-echo ------------- SPB (WEST RUSSIA)
+echo ------------- SPB (NORTH-WEST RUSSIA)
 ping spb.sandbox.infobox.ru
 echo ------------- KRASNOYARSK (SIBERIAN RUSSIA)
 ping krs.sandbox.infobox.ru
--- pong/pong.sh
+++ pong/pong.sh
@@ -4,32 +4,41 @@ ping -c 4 8.8.8.8
 ping -c 4 8.8.4.4
 echo ==========================
 echo PING LEVEL3
-ping -c 4  4.2.2.1
-ping -c 4  4.2.2.2
+ping -c 4 4.2.2.1
+ping -c 4 4.2.2.2
+# ping -c 4 4.2.2.3
+# ping -c 4 4.2.2.4
+# ping -c 4 4.2.2.5
+# ping -c 4 4.2.2.6
 echo ==========================
 echo PING AUSTRALIA ns1.telstra.net
-ping -c 4  139.130.4.5
+ping -c 4 139.130.4.5
 echo ==========================
 echo PING OpenDNS
-ping -c 4  208.67.222.222
-ping -c 4  208.67.220.220
+ping -c 4 208.67.222.222
+ping -c 4 208.67.220.220
+echo ==========================
+echo PING MSK-IX NTP stratum-1 server --- better not to do this often
+# ping -c 4 194.190.168.1
+echo ==========================
+echo next tests require DNS to be working
 echo ==========================
 echo PING YANDEX
-ping -c 4  ya.ru
+ping -c 4 ya.ru
 echo ==========================
 echo PING INFOBOX:
 echo ------------- AMSTERDAM (EUROPE)
-ping -c 4  ams.sandbox.infobox.ru
-echo ------------- SPB (WEST RUSSIA)
-ping -c 4  spb.sandbox.infobox.ru
+ping -c 4 ams.sandbox.infobox.ru
+echo ------------- SPB (NORTH-WEST RUSSIA)
+ping -c 4 spb.sandbox.infobox.ru
 echo ------------- KRASNOYARSK (SIBERIAN RUSSIA)
-ping -c 4  krs.sandbox.infobox.ru
+ping -c 4 krs.sandbox.infobox.ru
 echo ==========================
 echo PING WIKIPEDIA
-ping -c 4  wikipedia.org
+ping -c 4 wikipedia.org
 echo ==========================
 echo PING USA
-ping -c 4  whitehouse.gov
+ping -c 4 whitehouse.gov
 echo ==========================
 echo PONG complete
 echo ==========================
