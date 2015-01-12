46a8332f7ed251d8ca6e9c09fd776156a800f4ec
Denis Borchev <dborchev@gmail.com>
2015-01-12 15:36:24 +0300

    speed and multithread experiment
    a little speedup (though less verbose) of contactexpand.py;
    a multithread multicontactexpand.py as an experiment

--- contactexpand/contactexpand.py
+++ contactexpand/contactexpand.py
@@ -1,41 +1,50 @@
-import csv, time 
-timetorest = 0.5
-timestart = time.time()
-print('Opening...')
-contactsReader = csv.reader(open(r'c:\bin\pyth\contacts.csv'), delimiter=';')
-resultFile = open(r"output.csv",'wb')
-wr = csv.writer(resultFile, dialect='excel')
-rowrow = []
-rowrowrow=[]
-rownorow=[]
-countone=0
-for row in contactsReader:
-  countone= countone+1
-  rowrow.append(row)
-rowrow.pop(0) 
-countone= countone-1
-print('Contact list extracted.\n\nExpanding...')
-if countone>500: timetorest=0.05
-if countone>1000: timetorest=0
-time.sleep(timetorest) 
+import csv, time
 
-for row in rowrow:
-  rowrowrow.append(row[0:2])
-print('\nThere are '+str(countone)+' records to expand, it may take some time.')
-time.sleep(timetorest) 
-for row in rowrowrow:
-  for rowtwo in rowrow:
-    rownorow.append(row+rowtwo)
-    if rownorow[-1][0]==rownorow[-1][2]:
-	  rownorow.pop()
-print('\nExpanded to memory ok.\n\nNow writing output.csv some '+str((countone*countone)-countone)+' records.')
-time.sleep(timetorest*2) 
-counttwo=0
 
-for row in rownorow:
-  if counttwo/countone==counttwo%countone:
-    print('.'),
-    time.sleep(timetorest/100) 
-  wr.writerow(row)
-  counttwo= counttwo+1  
-print('\nDone expanding '+str(countone)+' records into '+str(counttwo)+' records in ' + str(format(time.time()-timestart-(timetorest*2)-(timetorest/100)*(counttwo/countone), '.2f')) + ' seconds')
+
+def main():
+  timetorest = 0 #no rest for the wicked
+  timestart = time.time()
+  print(time.strftime('[%d/%m %H:%M:%S]',time.localtime())+' Starting')
+  contactsReader = csv.reader(open(r'contacts.csv'), delimiter=';')
+  resultFile = open(r"output.csv",'wb')
+  global wr, rowrow, rowrowrow, rownorow
+  wr = csv.writer(resultFile, dialect='excel')
+  rowrow = []
+  rowrowrow = []
+  rownorow = []
+  countone = 0
+  for row in contactsReader:
+    countone= countone+1
+    rowrow.append(row)
+  rowrow.pop(0) 
+  countone = countone-1
+  print(time.strftime('[%d/%m %H:%M:%S]',time.localtime())+' Contact list extracted.\n\nExpanding...')
+  if countone>500: timetorest=0.05
+  if countone>1000: timetorest=0
+  time.sleep(timetorest) 
+  for row in rowrow:
+    rowrowrow.append(row[0:2])
+  print(time.strftime('[%d/%m %H:%M:%S]',time.localtime())+' There are '+str(countone)+' records to expand, it may take some time.')
+  time.sleep(timetorest) 
+  #print(time.strftime('[%d/%m %H:%M:%S]',time.localtime()))
+  for row in rowrowrow:
+    for rowtwo in rowrow:
+      rownorow.append(row+rowtwo)
+      if rownorow[-1][0]==rownorow[-1][2]:
+        rownorow.pop()
+  #print(time.strftime('[%d/%m %H:%M:%S]',time.localtime()))
+  print(time.strftime('[%d/%m %H:%M:%S]',time.localtime())+' Expanded to memory ok. Now writing to output.csv '+str((countone*countone)-countone)+' records...')
+  time.sleep(timetorest*2) 
+  #counttwo=0
+  #for row in rownorow:
+  #if counttwo/countone==counttwo%countone:
+  #print('.'),
+  #time.sleep(timetorest/100) 
+  wr.writerows(rownorow)
+  #counttwo= counttwo+1  
+  print(time.strftime('[%d/%m %H:%M:%S]',time.localtime())+' Done expanding '+str(countone)+' records into '+str((countone*countone)-countone)+' records in ' + str(format(time.time()-timestart-(timetorest*2)-(timetorest/100)*((((countone*countone)-countone))/countone), '.4f') + ' seconds'))
+
+if __name__ == '__main__': 
+  main()  
+  #EOF
---
+++ contactexpand/multicontactexpand.py
@@ -0,0 +1,53 @@
+'''
+  A multithread version of contactexpand.py
+  It is not very efficient. I made it just to try multithreading in Python.
+'''
+import csv
+from time import localtime, strftime, sleep
+import sys, locale, threading
+
+class expander(threading.Thread): 
+    def __init__ (self, table,multiplier):
+        self.table = table #table to expand
+        self.multiplier = multiplier #current multiplier
+        self.rowinrow = []
+        self.rowtow=[]
+        global rownorow		
+        threading.Thread.__init__(self)
+    def run (self):
+        global rownorow
+        global wr
+        for self.rowtwo in self.table:
+            self.rowinrow.append(self.multiplier+self.rowtwo)
+            if self.rowinrow[-1][0]==self.rowinrow[-1][2]:
+              self.rowinrow.pop()	
+        wr.writerows(self.rowinrow)
+        return True
+
+
+
+
+ 
+def main():
+  print(strftime('[%d/%m %H:%M:%S]',localtime())+' Starting')
+  contactsReader = csv.reader(open(r'contacts.csv'), delimiter=';')
+  resultFile = open(r'output.csv','wb')
+  global wr, rowrow, rowrowrow, rownorow
+  wr = csv.writer(resultFile, dialect='excel')
+  rowrow = [] #holds contacts.csv in usable form
+  rowrowrow = [] #holds 'bare' contacts
+  rownorow = [] #holds output.csv before writing to disk
+  for row in contactsReader:
+    rowrow.append(row)
+  rowrow.pop(0) 
+  for row in rowrow:
+    rowrowrow.append(row[0:2])
+  #print(strftime('[%d/%m %H:%M:%S]',localtime()))
+  for row in rowrowrow:
+    expander(table=rowrow,multiplier=row).start() #run as a separate thread
+  #print(strftime('[%d/%m %H:%M:%S]',localtime()))
+  print(strftime('[%d/%m %H:%M:%S]',localtime())+' Done')#writing is handled in threads
+
+if __name__ == '__main__': 
+  main()  
+ #EOF
