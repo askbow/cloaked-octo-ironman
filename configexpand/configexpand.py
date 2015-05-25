d599ffe8b4cd0a5c87f3fe442a0d6b45ffa13aa6
Denis Borchev <dborchev@gmail.com>
2015-05-25 17:40:44 +0300

    PEP8
    autoconvert via https://pypi.python.org/pypi/autopep8/1.1.1

--- configexpand/configexpand.py
+++ configexpand/configexpand.py
@@ -1,7 +1,7 @@
 '''
 This is the configexpand.py script.
 
-It is meant to generate config files for switches and routers, 
+It is meant to generate config files for switches and routers,
 but can be adapted to generate any iterative config.
 
 Two files are used as input:
@@ -14,36 +14,46 @@ One file on output:
 '''
 
 
-import csv, time
+import csv
+import time
 
 # to separate code and data, I load configs from this file:
 from configtemplate import *
 
+
 def configblock(dataline):
-  config = CONFIG_TEMPLATE_ITERATIVE
-  return config.format(**dataline)
+    config = CONFIG_TEMPLATE_ITERATIVE
+    return config.format(**dataline)
+
 
 def main():
-  print(time.strftime('[%d/%m %H:%M:%S]',time.localtime())+' Starting')
-  DataFileReader = csv.DictReader(open(r'data.csv'), delimiter=';')
-  print(time.strftime('[%d/%m %H:%M:%S]',time.localtime())+' Parameter list extracted. Generating config...')
-  sequence = CONFIG_TEMPLATE_COUNTER['offset'] # offset, if needed
-  config = CONFIG_TEMPLATE_ADD_BEFORE + "\n"
-  for line in DataFileReader:
-    line['sequence'] = str(sequence) # in iterative configs sequence number is often needed
-    line.update(CONFIG_TEMPLATE_STATIC)   # that way any static data can be added needed
-    sequence = sequence + 1
-    config = config + configblock(line)
-  config = config + "\n" + CONFIG_TEMPLATE_ADD_AFTER
-  print(time.strftime('[%d/%m %H:%M:%S]',time.localtime())+' Config generated. Writing to config.conf ...')
-  with open(r'config.conf','wb') as resultfile:
-    resultfile.write(config)
-  print(time.strftime('[%d/%m %H:%M:%S]',time.localtime())+' Done.')
-  
-  
-  
-  
-  
-if __name__ == '__main__': 
-  main()  
-  #EOF
+    print(time.strftime('[%d/%m %H:%M:%S]', time.localtime()) + ' Starting')
+    DataFileReader = csv.DictReader(open(r'data.csv'), delimiter=';')
+    print(
+        time.strftime(
+            '[%d/%m %H:%M:%S]',
+            time.localtime()) +
+        ' Parameter list extracted. Generating config...')
+    sequence = CONFIG_TEMPLATE_COUNTER['offset']  # offset, if needed
+    config = CONFIG_TEMPLATE_ADD_BEFORE + "\n"
+    for line in DataFileReader:
+        # in iterative configs sequence number is often needed
+        line['sequence'] = str(sequence)
+        # that way any static data can be added needed
+        line.update(CONFIG_TEMPLATE_STATIC)
+        sequence = sequence + 1
+        config = config + configblock(line)
+    config = config + "\n" + CONFIG_TEMPLATE_ADD_AFTER
+    print(
+        time.strftime(
+            '[%d/%m %H:%M:%S]',
+            time.localtime()) +
+        ' Config generated. Writing to config.conf ...')
+    with open(r'config.conf', 'wb') as resultfile:
+        resultfile.write(config)
+    print(time.strftime('[%d/%m %H:%M:%S]', time.localtime()) + ' Done.')
+
+
+if __name__ == '__main__':
+    main()
+    # EOF
