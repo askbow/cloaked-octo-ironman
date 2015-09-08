741be602c1fbabceace1322b31a8941f0cfd759b
Denis Borchev <dborchev@gmail.com>
2015-09-08 15:29:28 +0300

    csv coloumn remover
    Added new script - it removes fields from tables, if the field name
    contains a specified substring. Useful in CUCM administration.

---
+++ columnremover/remove.py
@@ -0,0 +1,38 @@
+import csv
+import time
+
+# We'll remove all fields (coloumns) off the input file, that contain this substring:
+LOOKUPSTRING = 'ASCII'
+
+def main():
+    print(time.strftime('[%d/%m %H:%M:%S]', time.localtime()) + ' Starting')
+    inputReader = csv.DictReader(open(r'input.csv'), delimiter=';')
+    # first - a quicker test, if we even hav what we're looking for:
+    stringfound = False
+    outfield = []
+    for field in inputReader.fieldnames:
+        if field.find(LOOKUPSTRING) >= 0:
+           stringfound = True
+        else:
+            outfield.append(field)
+    if stringfound:
+        # Now that we are sure, let's remove tham all, one by one:
+        wr = csv.DictWriter(open(r"output.csv", 'wb'), fieldnames=outfield, dialect='excel', delimiter=';')
+        wr.writeheader()
+        for row in inputReader:
+            outrow = {}
+            for field in inputReader.fieldnames:
+                if field.find(LOOKUPSTRING) < 0:
+                    outrow[field] = row[field]
+            if outrow != {}:
+                wr.writerow(outrow)
+    else:
+        print(time.strftime('[%d/%m %H:%M:%S]', time.localtime()) + "No fields contains that substring in the input file!")
+    print(time.strftime('[%d/%m %H:%M:%S]', time.localtime()) + ' Finished')
+
+
+
+
+if __name__ == '__main__':
+    main()
+    # EOF
