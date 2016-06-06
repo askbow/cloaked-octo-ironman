632e7d61953c803eff9edac0fb256bba8ba584bd
Denis Borchev <borchev@netube.ru>
2016-06-06 11:51:36 +0300

    create script to monitor memory

---
+++ misc/memmon.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# MEMMON.sh
+# A simple script to log process memory consumption
+#
+# Mostly based on http://stackoverflow.com/a/147322
+# some additions based on http://askubuntu.com/a/383874
+# 
+# Usage:
+# chmod +x memmon.sh
+# sudo ./memmon.sh
+#
+# Sudo: looks like it needs elevated priv. to display ps data on some systems
+#
+# Let it run for some time, then inspect the produced log.
+#
+
+echo "MEM mon script started"
+echo $(date)
+echo "================================="
+while true
+do
+echo "---------------------------------" >> /tmp/mem_usage
+date >> /tmp/mem_usage
+ps aux --sort -rss -eo pid,pmem,rss,vsz,comm | head -16 >> /tmp/mem_usage
+echo $(date)
+# in my case, it's a long-running issue, so need a longer sleep:
+sleep 4800
+done
