a2e21b77a880f6ed58d02d918a881e8844861717
Denis Borchev <dborchev@gmail.com>
2017-03-14 09:11:05 +0300

    start rewriting pong
    now the list of addresses to ping is loaded from a separate file. So
    much easier to maintain!
    also some code in preparation to rewrite python version

--- pong/pong.cmd
+++ pong/pong.cmd
@@ -1,45 +1,27 @@
 @echo off
-echo PING GOOGLE
-ping 8.8.8.8
-ping 8.8.4.4
+SET scriptpath=%~dp0
+
+rem TODO:
+rem simple way to check for python presense: https://stackoverflow.com/questions/4920483/batch-file-to-check-if-python-is-installed/26241114#26241114
+rem python --version 2>NUL
+rem if errorlevel 1 goto errorNoPython
+rem 
+rem rem here goes python
+rem python %scriptpath%pong.py
+rem 
+rem goto:eof
+rem :errorNoPython
+rem echo.
+rem echo Python not installed. Continue with standard ping:
 echo ==========================
-echo PING LEVEL3
-ping 4.2.2.1
-ping 4.2.2.2
-rem ping 4.2.2.3
-rem ping 4.2.2.4
-rem ping 4.2.2.5
-rem ping 4.2.2.6
+echo World-wide PING starting
 echo ==========================
-echo PING AUSTRALIA ns1.telstra.net
-ping 139.130.4.5
+
+for /F "tokens=1,*" %%A   in (%scriptpath%pinglist.txt) do @echo ========================== & @echo . & @echo     %%B & ping -w 500 -l 100 -n 3 %%A
+
 echo ==========================
-echo PING OpenDNS
-ping 208.67.222.222
-ping 208.67.220.220
+echo World-wide PING complete
 echo ==========================
-echo PING MSK-IX NTP stratum-1 server --- better not to do this often
-rem ping 194.190.168.1
-echo ==========================
-echo next tests require DNS to be working
-echo ==========================
-echo PING YANDEX
-ping ya.ru
-echo ==========================
-echo PING INFOBOX:
-echo ------------- AMSTERDAM (EUROPE)
-ping ams.sandbox.infobox.ru
-echo ------------- SPB (NORTH-WEST RUSSIA)
-ping spb.sandbox.infobox.ru
-echo ------------- KRASNOYARSK (SIBERIAN RUSSIA)
-ping krs.sandbox.infobox.ru
-echo ==========================
-echo PING WIKIPEDIA
-ping wikipedia.org
-echo ==========================
-echo PING USA
-ping whitehouse.gov
-echo ==========================
-echo PONG complete
-echo ==========================
- 
+echo next stage - long ping 8.8.8.8
+pause
+ping -w 50 -l 32 -t 8.8.8.8
